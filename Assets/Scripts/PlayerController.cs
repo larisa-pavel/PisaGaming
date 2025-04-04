873e7c1e1d1a75f24bc48c6288bdaf2c4edea3dd
Răzvan Burlacu <razvanburlacu11231@gmail.com>
2025-04-04 14:35:36 +0300

    movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
@@ -18,31 +17,6 @@ public class PlayerController : MonoBehaviour
     private Vector3 velocity;
     private float verticalLookRotation = 0f;
 
-    private PlayerInputActions inputActions;
-    private Vector2 moveInput;
-    private Vector2 lookInput;
-    private bool jumpInput;
-    private bool sprintInput;
-
-    void Awake()
-    {
-        inputActions = new PlayerInputActions();
-    }
-
-    void OnEnable()
-    {
-        inputActions.Enable();
-        inputActions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        inputActions.Player.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
-        inputActions.Player.Jump.performed += ctx => jumpInput = ctx.ReadValueAsButton();
-        inputActions.Player.Sprint.performed += ctx => sprintInput = ctx.ReadValueAsButton();
-    }
-
-    void OnDisable()
-    {
-        inputActions.Disable();
-    }
-
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -54,15 +28,16 @@ public class PlayerController : MonoBehaviour
         Move();
         Look();
     }
-
     bool IsGrounded()
     {
         return Physics.Raycast(transform.position, Vector3.down, (controller.height / 2f) + 0.1f);
     }
-
     void Move()
     {
-        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * moveSpeed * Time.deltaTime);
 
         if (controller.isGrounded && velocity.y < 0)
@@ -70,24 +45,23 @@ public class PlayerController : MonoBehaviour
             velocity.y = -2f;
         }
 
-        if (jumpInput && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
-
-        if (sprintInput)
+        if (Input.GetButtonDown("E"))
         {
             moveSpeed /= 2;
         }
 
         velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime); 
     }
 
     void Look()
     {
-        float mouseX = lookInput.x * mouseSensitivity;
-        float mouseY = lookInput.y * mouseSensitivity;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         verticalLookRotation -= mouseY;
         verticalLookRotation = Mathf.Clamp(verticalLookRotation, -maxLookAngle, maxLookAngle);
