1d1dea282972dc6d91385f3550dcc38395392ce1
mayushiiii <marymovileanu@gmail.com>
2025-04-04 16:25:09 +0300

    added river initial lvl

---
+++ Assets/ObstacleMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 1000000f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+}
